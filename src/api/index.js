521efa05c3fa039807a4b13713f204c006c4164b
Rodneysia <rodneysia.musgraves@gmail.comgit config --global user.name Rodneysiagit config --global user.email rodneysia.musgraves@gmail.com>
2022-12-20 00:08:11 -0600

    Fetched posts done

--- src/api/index.js
+++
@@ -1,8 +0,0 @@
-
-fetch('https://strangers-things.herokuapp.com/api/2209-FTB-MT-WEB-PT/posts/')
-  .then (res => {
-    return res.json()
-  }
-  )
-  .then (data => console.log(data))
-  .catch(error => console.log("ERROR"))
--- src/index.js
+++ src/index.js
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './api/index';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+    const [postId, setPostId] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    console.log('posts', posts);
+
+    useEffect(() => {
+      const fetchPosts = async () => {
+        const resp = await fetch('https://strangers-things.herokuapp.com/api/2209-FTB-MT-WEB-PT/posts/')
+       .then ( (resp) => resp.json () )
+       
+      // console.log('resp:', resp);
+       setPosts(resp.data.posts);
+     
+  
+
+      }
+      fetchPosts();
+    }, []);
+
+  return <>
+    <h1>
+      Posts
+    </h1>
+    {
+        posts.map(post => (<div key={post.id}>
+        <h3>{post.title}</h3>
+        <div>{post.description}</div>
+      </div>))
+    }
+    </>
+  
+  }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <App />,
   </React.StrictMode>
+
 );
 
-reportWebVitals();
+export default App
